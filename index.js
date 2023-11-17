795fb10cd81b2c42e0a4181f4913671a001815ac
XNS-ivy <ayramusic.nightcore@gmail.com>
2023-11-17 09:55:52 +0700

    new improve code on index

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const pino = require("pino");
 const { handleMessages } = require("./src/handlingMessage");
 const useCd = process.argv.includes("--code");
 const Boom = require('@hapi/boom');
+// --------------------
+const SESSION_FILE_PATH = "./session";
+const PHONE_NUMBER_PROMPT_DELAY = 3000
 
 async function start() {
   try {
-  const { state, saveCreds } = await useMultiFileAuthState("./session");
+  const { state, saveCreds } = await useMultiFileAuthState(SESSION_FILE_PATH);
+  
   const Naori = makeWASocket({
     logger: pino({ level: "silent" }),
     printQRInTerminal: !useCd, 
@@ -28,24 +32,27 @@ async function start() {
     });
 
     const phoneNumber = await question();
-    setTimeout(async function() {
-      const codePair = await Naori.requestPairingCode(phoneNumber);
-      console.log("Connect With Pairing Code : " + codePair);
-    }, 3000);
+  setTimeout(async () => {
+  const codePair = await Naori.requestPairingCode(phoneNumber);
+  console.log("Connect With Pairing Code : " + codePair);
+}, PHONE_NUMBER_PROMPT_DELAY);
   }
-
-  Naori.ev.on('connection.update', async ({ connection }) => {
+const handleConnectionUpdate = async ({ connection }) => {
     if (connection === "open") {
-      console.log("Connected: " + Naori.user.id.split(":")[0]);
+    console.log("Connected: " + Naori.user.id.split(":")[0]);
   } else if (connection === "close") {
-      try { 
-        await start();
-      } catch (error){
-        throw Boom.badImplementation('Error : ', error);
+    try {
+      Naori.ev.off('connection.update', handleConnectionUpdate);
+      await start();
+    } catch (error) {
+      throw Boom.badImplementation('Error: ', error);
       }
     }
-  });
+  };
+  
+  Naori.ev.on('connection.update', handleConnectionUpdate);
   Naori.ev.on('creds.update', saveCreds);
+  
   Naori.ev.on('messages.upsert', ({ messages }) => {
     if (messages && messages.length > 0) {
       const m = messages[0];
@@ -55,7 +62,6 @@ async function start() {
   });
 } catch (error){
   console.error('Error in start function:', error);
+  }
 }
-}
-
 start();
